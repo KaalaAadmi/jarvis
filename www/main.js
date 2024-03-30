36066336193d5a64bb66f2b4a0031d2633366829
Arnav <arnav264@gmail.com>
2024-03-30 20:33:55 +0000

    third commit

--- www/main.js
+++ www/main.js
@@ -9,4 +9,28 @@ $(document).ready(function () {
       effect: "bounceOut",
     },
   });
+  // siri wave configuration
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 640,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.50",
+    autostart: true,
+  });
+
+  // siri message animation
+  $(".siri-message").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "fadeInUp",
+      sync: true,
+    },
+    out: {
+      effect: "fadeOutUp",
+      sync: true,
+    },
+  });
 });
